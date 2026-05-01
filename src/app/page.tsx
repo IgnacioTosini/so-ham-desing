4dc3aec189462cbd884b9aa82909c5e0733854a0
ignacio tosini <ignaciotosini2002@gmail.com>
2026-05-01 20:21:06 -0300

    fix vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { getProducts } from "@/actions/product.action";
 import { getStones } from "@/actions/stone.action";
 import { AboutBrand, CreateYourPiece, Hero, ViewPieces } from "@/components/sections";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const products = await getProducts();
   const stones = await getStones();
